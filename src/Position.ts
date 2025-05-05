c6b28f48efb70ae1f005d9b38b9de681155a58ba
Paul van Cappelle <paul.vancappelle@ns.nl>
2025-05-05 14:54:06 +0200

    refactor: remove redundant comment from Position

--- src/Position.ts
+++ src/Position.ts
@@ -1,5 +1,5 @@
 export class Position {
   x: number = 0;
   y: number = 0;
-  direction: string = 'N'; // 'char' in C# is effectively a one-character string in TypeScript
+  direction: string = 'N';
 }
